c6e209b8fe211dea08e080cd5fc55e4a82651b00
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-01-13 14:06:30 +0400

    Добавьте файлы проекта.

---
+++ EmailSave/EMailList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace EmailSave
+{
+    [Serializable]
+    class EMailList
+    {
+        public List<EMail> EMails
+        {
+            get; private set;
+        }
+
+        public EMailList()
+        {
+            EMails = Run();
+        }
+
+        private List<EMail> Run()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    var list = (List<EMail>)formatter.Deserialize(fs);
+                    if (list != null)
+                        return list;
+                    else
+                        return new List<EMail>();
+                }
+            }
+            catch
+            { return new List<EMail>(); }
+
+        }
+
+        private void Save()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(fs, EMails);
+                }
+            }
+            catch { }
+        }
+
+        public void Add(string em)
+        {
+            var newEmail = new EMail(em);
+            if (newEmail != null)
+            {
+                EMails.Add(newEmail);
+                Save();
+            }
+        }
+
+        public void Clear(EMail eMail)
+        {
+            EMails.Remove(eMail);
+            Save();
+        }
+
+        public void ClearAll()
+        {
+            EMails.Clear();
+            Save();
+        }
+    }
+}
---
+++ EmailSave/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailSave
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        EMailList eMail;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+        private void Start()
+        {
+            eMail = new EMailList();
+           dgEmail.ItemsSource = eMail.EMails;
+        }
+
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                eMail.Add(tbMail.Text);
+                Start();
+            }
+            catch
+            {
+                MessageBox.Show("Неверный  емай");
+                return;
+            }
+        }
+
+        private void btClear_Click(object sender, RoutedEventArgs e)
+        {
+            eMail.ClearAll();
+            Start();
+        }
+    }
+}
